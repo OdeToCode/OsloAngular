55f6a039e8a8b4a3ebc1bda7ede636e796fef574
K. Scott Allen <scott@odetocode.com>
2014-02-12 07:06:30 -0500

    simulating async movie service

--- movieApp/app.js
+++ movieApp/app.js
@@ -1,28 +1,34 @@
-﻿(function() {
+﻿(function () {
 
     var module = angular.module("movieApp", []);
 
-    module.run(function($rootScope) {
+    module.run(function ($rootScope) {
         $rootScope.message = "Hello, World";
     });
-   
+
 }());
 
 (function () {
 
     var module = angular.module("movieApp");
 
-    var MovieListController = function($scope) {
+    var MovieListController = function ($scope, movieService) {
+
+        var onError = function(error) {
+            $scope.error = error;
+        };
+
+        var showMovies = function(response) {
+            $scope.movies = response.data;
+        };
 
         $scope.message = "Hello from Movie List Controller";
 
-        $scope.movies = [
-            { title: "Star Wars", length: 120 },
-            { title: "Gravity", length: 90 },
-            { title: "The Hobbit 2", length: 240 }
-        ];
+        movieService
+             .getAll()
+              .then(showMovies, onError);
 
-        $scope.makeLonger = function(movie) {
+        $scope.makeLonger = function (movie) {
             movie.length += 1;
         };
 
@@ -33,5 +39,40 @@
     };
 
     module.controller("MovieListController", MovieListController);
+
+}());
+
+(function () {
+
+    var module = angular.module("movieApp");
+
+    var movieService = function ($http, $q, $timeout) {
+        
+        var get = function () {
+            
+            var deferred = $q.defer();
+
+            var movies =  [
+                { "title": "Star Wars 4", "length": 120 },
+                { "title": "Gravity", "length": 90 },
+                { "title": "The Hobbit 2", "length": 240 }
+            ];
+
+            $timeout(function() {
+                deferred.resolve(movies);
+            }, 3000);       
+           
+            return deferred.promise;
+
+            //return $http.get("movies.json");
+        };
+
+        return {            
+          getAll: get  
+        };
+
+    };
+
+    module.factory("movieService", movieService);
 
 }());
