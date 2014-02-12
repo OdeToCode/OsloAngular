11eb94c269857456ef1b72698c526129ce18a5e7
K. Scott Allen <scott@odetocode.com>
2014-02-12 05:31:14 -0500

    showing some movies

--- movieApp/app.js
+++ movieApp/app.js
@@ -5,6 +5,30 @@
     module.run(function($rootScope) {
         $rootScope.message = "Hello, World";
     });
+   
+}());
 
+(function () {
+
+    var module = angular.module("movieApp");
+
+    var MovieListController = function($scope) {
+
+        $scope.message = "Hello from Movie List Controller";
+
+        $scope.movies = [
+            { title: "Star Wars", length: 120 },
+            { title: "Gravity", length: 90 },
+            { title: "The Hobbit 2", length: 240 }
+        ];
+
+        $scope.addExclamation = function(movie) {
+            movie.title += "!";
+        };
+
+
+    };
+
+    module.controller("MovieListController", MovieListController);
 
 }());
